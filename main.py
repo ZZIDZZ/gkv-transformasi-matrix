c93533da810aa4140bccd7585edb0c5dfead168e
Reylyer <reylyercross@gmail.com>
2022-03-31 00:45:08 +0700

    staging 1

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 from manim import *
+import numpy as np
 
-class Transformasi(ThreeDScene):
+class Transformasi(ThreeDScene, MovingCamera):
     def __init__(self):
         super().__init__()
 
     def construct(self):
-        self.move_camera(phi=45*DEGREES, theta=45*DEGREES, focal_distance=5)
+
         axis = ThreeDAxes()
-        self.play(FadeIn(axis))
-        dode = Dodecahedron()
-        dode.set_stroke(BLUE_B, opacity=1.0)
-        dode.set_fill(BLUE_B, opacity=1.0)
+        labz = axis.get_z_axis_label(Tex("$z$-label"))
 
-        self.play(DrawBorderThenFill(dode))
-        self.play(dode.animate.shift(RIGHT))
-        self.play(Rotate(dode, angle=PI/3, about_point=ORIGIN, rate_func=smooth))
-        self.play(dode.animate.shift(RIGHT))
+        cu = Cube(side_length=1)
+        cu.shift(RIGHT + 2*UP)
+        cu.set_stroke(BLUE_B, opacity=1.0)
+        cu.set_fill(BLUE_B, opacity=1.0)
 
-        self.wait()
+        vertices = cu.get_all_points()
+
+        self.set_camera_orientation(phi=60*DEGREES, theta=-45*DEGREES, focal_distance=10)
+
+        self.play(FadeIn(axis), Write(labz))
+        self.play(DrawBorderThenFill(cu))
+        self.wait(0.5)
+
+        # https://gamedev.stackexchange.com/questions/72528/how-can-i-project-a-3d-point-onto-a-3d-line
+        rot_axis = Line3D([2, 1, 0], [3, 3, 1])
+        self.play(Write(rot_axis))
+
+        AB = rot_axis.end - rot_axis.start
+        AP = cu.get_center() - rot_axis.start
+
+        point_rot = rot_axis.start + np.dot(AP, AB) / np.dot(AB, AB) * AB
+        self.begin_ambient_camera_rotation(45*DEGREES/3, about='theta')
+        self.play(Rotate(cu, angle=10*360*DEGREES, axis=AB, about_point=point_rot, run_time=15, rate_func=linear))
+    
+        self.wait(5)
+        self.stop_ambient_camera_rotation(about='theta')
